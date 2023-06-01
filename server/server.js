198b03d6b3bbb34df5b0efe60b72683903caf7e6
Jrcrz <jcruz.codeio@gmail.com>
2023-06-01 15:48:05 -0700

    minor change on server.js

--- server/server.js
+++ server/server.js
@@ -6,8 +6,8 @@ const app = express();
 const scrumRouter = require('./routes/scrumRouter');
 const userRouter = require('./routes/userRouter');
 
-const PORT = 3000;
 
+const PORT = 3000; 
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
